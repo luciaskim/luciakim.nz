f9e77e0ab6a203f17878cadd8dc98b01fa7041a0
Lucia <luciakimnz@gmail.com>
2022-06-22 13:07:34 +1200

    changed indentation of <title> tag

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -30,37 +30,6 @@ const ptComponents = {
   }
 }
 
-const query = groq`*[_type == "post" && slug.current == $slug][0]{
-  title,
-  "categories": categories[]->title, 
-  mainImage, 
-  publishedAt,
-  body
-}`
-
-export async function getStaticPaths() {
-    const paths = await client.fetch(
-      groq`*[_type == "post" && defined(slug.current)][].slug.current`
-    )
-  
-    return {
-      paths: paths.map((slug) => ({params: {slug}})),
-      fallback: true,
-    }
-  }
-  
-
-export async function getStaticProps(context) {
-    // It's important to default the slug so that it doesn't return "undefined"
-    const { slug = "" } = context.params
-    const post = await client.fetch(query, { slug })
-    return {
-      props: {
-        post
-      }
-    }
-  }
-
 
 
 const Post = ({post}) => {
@@ -68,7 +37,7 @@ const Post = ({post}) => {
   return (
     <Layout>
       <Head>
-          <title>{title}</title>
+        <title>{title}</title>
       </Head>
       <article>
         <h1>{title}</h1>
@@ -92,7 +61,33 @@ const Post = ({post}) => {
   )
 }
 
+const query = groq`*[_type == "post" && slug.current == $slug][0]{
+  title,
+  "categories": categories[]->title, 
+  mainImage, 
+  publishedAt,
+  body
+}`
 
+export async function getStaticPaths() {
+  const paths = await client.fetch(
+    groq`*[_type == "post" && defined(slug.current)][].slug.current`
+  )
 
+  return {
+    paths: paths.map((slug) => ({params: {slug}})),
+    fallback: true,
+  }
+}
 
+export async function getStaticProps(context) {
+  // It's important to default the slug so that it doesn't return "undefined"
+  const { slug = "" } = context.params
+  const post = await client.fetch(query, { slug })
+  return {
+    props: {
+      post
+    }
+  }
+}
 export default Post;
